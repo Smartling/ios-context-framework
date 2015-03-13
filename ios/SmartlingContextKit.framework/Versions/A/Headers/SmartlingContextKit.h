740d894251395004443fee32c67665130e925d50
Scott Rossillo <srossillo@smartling.com>
2015-03-13 14:54:55 -0400

    Add back framework

---
+++ ios/SmartlingContextKit.framework/Versions/A/Headers/SmartlingContextKit.h
@@ -0,0 +1,15 @@
+//
+//  SmartlingContextKit.h
+//  SmartlingContextKit
+//
+//  Created by Scott Rossillo on 11/4/14.
+//  Copyright (c) 2014 Smartling, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmartlingContextKit.
+FOUNDATION_EXPORT double SmartlingContextKitVersionNumber;
+
+//! Project version string for SmartlingContextKit.
+FOUNDATION_EXPORT const unsigned char SmartlingContextKitVersionString[];
